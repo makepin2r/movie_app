bafeaf44b8051c6dd25c621eb6cd49ddc9ccf113
Lucy <makepin2r@gmail.com>
2021-02-08 16:56:36 +0900

    #2-2 map 이용해 동적으로 컴포넌트 생성하기 실습

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-function Food(props) {
-  return <h3>I Like {props.name}</h3>;
+function Food({name, pic}) {
+  return <div>
+    <h2>I Like {name}</h2>
+    <img src={pic} alt=""/>
+  </div>;
 }
+
+const foodILike = [
+  {
+    name: "taco",
+    image: "https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8dGFjb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "ramen",
+    image: "https://images.unsplash.com/photo-1591814468924-caf88d1232e1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFtZW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "chocolate",
+    image: "https://images.unsplash.com/photo-1599599810769-bcde5a160d32?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvY29sYXRlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "pasta",
+    image: "https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFzdGF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+  }
+];
 
 function App() {
   return <div>
-    <h1>Hello</h1>
-    <Food name="kimchi" />
-    <Food name="ramen" />
-    <Food name="taco" />
+      {foodILike.map(dish => <Food name={dish.name} pic={dish.image}/>)}
   </div>;
 }
 

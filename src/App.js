6d62ad48951473d0045e799289b5b49cf1496901
Lucy <makepin2r@gmail.com>
2021-02-09 16:23:16 +0900

    #3-0 클래스 컴포넌트와 state 기초

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-import PropTypes from "prop-types";
+import React from "react";
+// import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id:1,
-    name: "taco",
-    image: "https://images.unsplash.com/photo-1599974579688-8dbdd335c77f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8dGFjb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
-    rating: 5.0
-  },
-  {
-    id:2,
-    name: "ramen",
-    image: "https://images.unsplash.com/photo-1591814468924-caf88d1232e1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFtZW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
-    rating: 5.0
-  },
-  {
-    id:3,
-    name: "chocolate",
-    image: "https://images.unsplash.com/photo-1599599810769-bcde5a160d32?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y2hvY29sYXRlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
-    rating: 5.0
-  },
-  {
-    id:4,
-    name: "pasta",
-    image: "https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFzdGF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
-    rating: 5.0
+// class component
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    console.log("Add");
+  };
+  minus = () => {
+    console.log("Minus");
+  };
+
+  render(){
+    return <div>
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>;
   }
-];
 
-function Food({name, pic, rating}) {
-  return <div>
-    <h2>I Like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={pic} alt={name}/>
-  </div>;
-}
-
-// prop types
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-}
-
-function App() {
-  return <div>
-      {/* {console.log(foodILike.map(renderFood) )} */}
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} pic={dish.image} rating={dish.rating}/>)}
-  </div>;
 }
 
 export default App;

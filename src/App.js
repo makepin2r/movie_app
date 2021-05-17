757848864204b33a12250516c7979a9712ba1e17
Lucy <makepin2r@gmail.com>
2021-05-17 17:52:22 +0900

    Home, About Route 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App(){
-  return <span>dd</span>;
+  return (
+    <HashRouter>
+      <Route path="/about" exact={true} component={About} />
+      <Route path="/home" exact={true} component={Home} />
+    </HashRouter>
+  );
 }
 
 export default App;

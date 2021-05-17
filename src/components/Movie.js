5aa87b0dd4415112e2c75d4361745c5d768f7014
Lucy <makepin2r@gmail.com>
2021-05-17 17:10:42 +0900

    react-router-dom 설치, 라우터를 위한 디렉토리 정리

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie( {year, title, summary, poster, genres} ) {
     return <div className="movie">
         <img src={poster} alt={title} title={title}/>
         <div className="movie__data">
-            <h3 className="movie__tiutle">{title}</h3>
+            <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
             <ul className="genres">
                 {genres.map((genre, index) => <li 

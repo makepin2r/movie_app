67e5c62ede206412f6aa3606a871d57b98a0a5ac
Lucy <makepin2r@gmail.com>
2021-05-20 17:53:01 +0900

    Navigation 생성

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App(){
   return (
     <HashRouter>
+      <Navigation/>
       <Route path="/about" exact={true} component={About} />
       <Route path="/home" exact={true} component={Home} />
     </HashRouter>
